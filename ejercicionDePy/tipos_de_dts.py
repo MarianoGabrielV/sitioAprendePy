b392de72562c859d2bde232ddbca2dafa6c962c8
MarianoVergniaud <mariano.unla@gmail.com>
2024-05-26 12:44:09 -0300

    primer

---
+++ ejercicionDePy/tipos_de_dts.py
@@ -0,0 +1,19 @@
+#variables String
+nombre = "Mi nombre"
+print(nombre)
+#variable Integer
+numero = 10
+print(numero)
+#variable Boolean
+hombre = True
+print(hombre)
+
+#suma de numeros
+print (3+3)
+#multiplicacion de numeros
+print (3*3)
+#division de numeor
+print (10/2)
+#divioson de numero enterio
+print(10//2)
+
