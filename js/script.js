e6bcb0db7fa9074a3fb9e27b8074e9dafd4090ab
MarianoVergniaud <mariano.unla@gmail.com>
2024-05-28 06:58:16 -0300

    renueva la pantalla con nuevos ejercicios

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function mostrarEjercicios(ejercicioId) {
+    // Ocultar todos los contenedores de ejercicios
+    const contenedores = document.querySelectorAll('.container.mt-1');
+    contenedores.forEach(contenedor => {
+        contenedor.style.display = 'none';
+    });
+
+    // Mostrar el contenedor de ejercicios seleccionado
+    const ejercicio = document.getElementById(ejercicioId);
+    if (ejercicio) {
+        ejercicio.style.display = 'block';
+    }
+
+    // Mostrar la imagen principal
+    document.getElementById('mainContent').style.display = 'block';
+}
